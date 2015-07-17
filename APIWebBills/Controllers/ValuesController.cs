4fe6d97e987f1cddc2e445b1be4e0506b20c9e9b
Karol <karolpyrek1@tlen.pl>
2015-07-18 00:47:11 +0200

    Poprawka

--- APIWebBills/Controllers/ValuesController.cs
+++ APIWebBills/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace APIWebBills.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value23" };
         }
 
         // GET api/values/5
