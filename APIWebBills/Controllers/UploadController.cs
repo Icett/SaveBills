d6eb7977d953f678aac9982b1efeb0e4ba55e605
Karol Pyrek <karolpyrek1@tlen.pl>
2015-10-27 16:25:01 +0200

    DOdanie upload

---
+++ APIWebBills/Controllers/UploadController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace APIWebBills.Controllers
+{
+    public class UploadController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public async Task<HttpResponseMessage> Post()
+        {
+            Stream requestStream = await this.Request.Content.ReadAsStreamAsync();
+            byte[] byteArray = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                await requestStream.CopyToAsync(ms);
+                byteArray = ms.ToArray();
+            }
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
